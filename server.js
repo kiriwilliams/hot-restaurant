7b6bca7167cb87638f93ef6e773aec0061ae8e38
sharky <zeebing@gmail.com>
2019-04-30 13:34:53 -0400

    added basic pathing

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+
+//Default route
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get("/reserve", function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
 //START SERVER
 app.listen(PORT, function(){
     console.log("App listening on PORT "+ PORT);
